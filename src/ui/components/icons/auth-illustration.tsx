e014141952149c13e33bf32b3b353363f6bc2fd3
Abner Persio <binhopersio@gmail.com>
2024-12-04 17:32:35 -0300

    refactor: component props

--- src/ui/components/icons/auth-illustration.tsx
+++ src/ui/components/icons/auth-illustration.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import { ComponentProps } from 'react';
 import authIllustration from '../../../assets/images/auth-illustration.svg';
 
-export default function AuthIllustration(props: React.ImgHTMLAttributes<HTMLImageElement>) {
+export default function AuthIllustration(props: ComponentProps<'img'>) {
   return <img src={authIllustration} {...props} />;
 }
--- src/ui/components/icons/logo.tsx
+++ src/ui/components/icons/logo.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import { ComponentProps } from 'react';
 
-export function Logo(props: React.SVGAttributes<SVGSVGElement>) {
+export function Logo(props: ComponentProps<'svg'>) {
   return (
     <svg viewBox="0 0 139 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
