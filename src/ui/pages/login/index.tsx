ceca4099ef9a6261f1df39006529ca5002c94543
Abner Persio <binhopersio@gmail.com>
2024-12-05 08:58:31 -0300

    feat: implement login and register pages ui

--- src/ui/pages/login/index.tsx
+++ src/ui/pages/login/index.tsx
@@ -1,3 +1,36 @@
+import { Link } from 'react-router-dom';
+import { routes } from '../../../infra/constants/routes';
+import { classNames } from '../../../shared/utils/class-names';
+import { Button } from '../../components/button';
+import { Input } from '../../components/input';
+
 export function LoginPage() {
-  return <>Login</>;
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">Entre em sua conta</h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">Novo por aqui?</span>
+
+          <Link
+            className={classNames('font-medium text-teal-900 tracking-[-0.5px]')}
+            to={routes.register}
+          >
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+
+      <form noValidate className="mt-[60px] flex flex-col gap-4">
+        <Input name="email" label="Email" type="email" />
+
+        <Input name="password" label="Senha" type="password" />
+
+        <Button type="submit" className="mt-2">
+          Entrar
+        </Button>
+      </form>
+    </>
+  );
 }
--- src/ui/pages/register/index.tsx
+++ src/ui/pages/register/index.tsx
@@ -1,3 +1,38 @@
+import { Link } from 'react-router-dom';
+import { routes } from '../../../infra/constants/routes';
+import { classNames } from '../../../shared/utils/class-names';
+import { Button } from '../../components/button';
+import { Input } from '../../components/input';
+
 export function RegisterPage() {
-  return <>Register</>;
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">Crie sua conta</h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">Já possui uma conta?</span>
+
+          <Link
+            className={classNames('font-medium text-teal-900 tracking-[-0.5px]')}
+            to={routes.login}
+          >
+            Fazer login
+          </Link>
+        </p>
+      </header>
+
+      <form noValidate className="mt-[60px] flex flex-col gap-4">
+        <Input name="name" label="Nome" />
+
+        <Input name="email" label="Email" type="email" />
+
+        <Input name="password" label="Senha" type="password" />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
+  );
 }
