fdd4189d17acfedd512060ff58164c2a2def2b94
Abner Persio <binhopersio@gmail.com>
2024-12-05 22:16:39 -0300

    feat: dashboard ui page

--- src/ui/pages/dashboard/index.tsx
+++ src/ui/pages/dashboard/index.tsx
@@ -1,3 +1,16 @@
+import { useAuth } from '../../../app/hooks/use-auth';
+import { Button } from '../../components/button';
+
 export function DashboardPage() {
-  return <>Dashboard</>;
+  const { signout } = useAuth();
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+
+      <Button type="button" onClick={signout}>
+        Sair
+      </Button>
+    </>
+  );
 }
